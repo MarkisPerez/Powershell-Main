65457d8fcb0a8fd5793f4e78a4eb0bcf424a59e5
MarkisPerez <44183206+MarkisPerez@users.noreply.github.com>
2018-10-30 10:33:20 -0500

    Add files via upload

--- Lab Unit 6.ps1
+++ Lab Unit 6.ps1
@@ -20,8 +20,8 @@ Function PrinterInfo{
     Set-Location "C:\Users\Temp"  
     $PrintList = Get-Printer | Select-Object -Property Name #Retrieves the name property from the list of printers
     foreach($printer in $PrintList){ #loops for the amount of printers stored in $PrintList
-        $name = $printer.Name #Assigns this variable the "Name" property value of whichever object it is on
-        New-Item -ItemType 'Directory'-name "$name" #Creates a directory
+        $name = $printer.Name 
+        New-Item -ItemType 'Directory'-name "$name" 
         $PrintInfo = Get-Printer -Name "$name" | Format-List #Pulls more detailed information about each printer through the loop
         $Printinfo | Out-File -FilePath "C:\Users\Temp\$name\$name.json" #Creates a JSON file in the indicated Path.
     }
