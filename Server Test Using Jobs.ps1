357b2529aff72b76dfdd27e638d02a71a4669c21
MarkisPerez <44183206+MarkisPerez@users.noreply.github.com>
2018-11-26 23:52:58 -0600

    Add files via upload

---
+++ Server Test Using Jobs.ps1
@@ -0,0 +1,21 @@
+﻿$choice = read-host 'Do you want to proceed? [y/n]'
+while ($choice -ne 'y'){
+    if($choice -eq 'n'){exit}
+    Write-Output 'Do you wish to proced [y/n]'
+}
+
+
+$servtest = Get-Content serverlist.csv
+
+
+$TestJob = {
+    foreach($url in $servtest){
+    If($url -eq 'Servers'){continue}
+    
+    Test-Connection "$url"
+    Receive-Job
+    }
+}
+
+
+Start-Job $TestJob
